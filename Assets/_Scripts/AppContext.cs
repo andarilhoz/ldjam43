af2c5e662c8c70e1d62aa85ce8b6757edd0347ed
Magno Gouveia <owlflee@gmail.com>
2018-12-01 17:12:40 -0200

    Add dependency injection and first screen

---
+++ Assets/_Scripts/AppContext.cs
@@ -0,0 +1,20 @@
+﻿using Injection;
+using UnityEngine;
+
+namespace Monobehaviours
+{
+    public class AppContext : MonoBehaviour
+    {
+        private static Injector Injector = new Injector();
+
+
+        public static void Inject(object script)
+        {
+            Injector.Inject(script);
+        }
+
+        private void Awake()
+        {
+        }
+    }
+}
